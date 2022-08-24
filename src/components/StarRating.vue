f57f9cd3e99affaed878ad7f43dfb97a3b48348e
ithiel883 <ajuka.zephiniah@gmail.com>
2022-08-24 23:27:14 +0100

    initial commit

---
+++ src/components/StarRating.vue
@@ -0,0 +1,45 @@
+<template>
+    <div v-if="isFloat(stars)">
+        <span class="material-icons my-auto fs-14  text-warning" v-for="star in checkInt(stars)">
+            grade
+        </span>
+        <span class="material-icons my-auto fs-14  text-warning">
+            star_half
+        </span>
+    </div>
+    <div v-else>
+        <span class="material-icons my-auto fs-14  text-warning" v-for="star in stars">
+            grade
+        </span>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+
+interface Props {
+    stars: number
+}
+
+const props = withDefaults(defineProps<Props>(), {
+
+});
+
+
+const isFloat = (val: number) => {
+    if (typeof val === 'number' && !Number.isNaN(val) && !Number.isInteger(val)) {
+        return true
+    }
+    return false
+}
+
+const checkInt = (val: number) => {
+    return Math.floor(val)
+}
+
+
+
+</script>
+
+<style scoped lang="scss">
+</style>
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import StarRating from './components/StarRating.vue'
+
+export default {
+    install: (app, options) => {
+        app.component()
+    },
+}
